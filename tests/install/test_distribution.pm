cdeb373671515681503900fa9df7dc362c84c77a
nicksinger <nsinger@suse.de>
2026-09-01 00:31:54 +0200

    feat: Make fetchneedles fail on interactive login prompts
    We expect https clones of public repositories to happen without
    authentication but apparently these requests can happen anytime
    so we should explicitly disable that feature.

--- tests/install/test_distribution.pm
+++ tests/install/test_distribution.pm
@@ -7,11 +7,11 @@ sub run {
     diag('assuming to be in terminal');
     if (get_var('FULL_OPENSUSE_TEST')) {
         diag('initialize working copy of openSUSE tests distribution with correct user');
-        assert_script_run('retry -s 30 -- sh -c "username=bernhard email=bernhard@susetest /usr/share/openqa/script/fetchneedles"', 3600);
+        assert_script_run('retry -s 30 -- sh -c "GIT_ASKPASS= GIT_TERMINAL_PROMPT=false username=bernhard email=bernhard@susetest /usr/share/openqa/script/fetchneedles"', 3600);
         save_screenshot;
     }
     # os-autoinst-distri-opensuse is changing quickly so it is likely to have
-    # changed within the 10 minute default refresh deadline of zypper.
+    # changed within the 10 minute default refresh timeout of zypper.
     # Therefore we try to refresh explicitly (with retries, in case of problems).
     install_packages('os-autoinst-distri-opensuse-deps');
     # leave a clean root console for the subsequent test module (if it needs x11, it should switch itself)
